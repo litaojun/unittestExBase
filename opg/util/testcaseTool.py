ea0cf4c5ccdf97d8c474de4509754f6045caeb60
li.taojun <li.taojun@LITAOJUN-NB.opg.cn>
2017-07-26 18:54:08 +0800

    sssss

--- opg/util/testcaseTool.py
+++ opg/util/testcaseTool.py
@@ -8,13 +8,15 @@ Created on 2017年7月9日
 import os
 from fileOper import walk_dir_test
 from opg.util.csvtools import  csvReadToDict, dictToInfaceDict
-
+from isSystemType import splict,getPlatfromType
+s = splict
+print "s=",s
 #path  测试用例文件所在根目录
 #desc  读取相对路径path目录下的所有测试用例，并将测试用例转换为{"interfacename":{"method":[["caseid","interfaceName","testPoint","preConditions","operationSteps","testData","expectedResult","actualResult"][]...[]]}}格式
 def creatTestCaseByPath(path="../../../../"):
     #获取相对path所在的绝对路径
     #pathcase = os.path.abspath(path+"\\"+"testcase")
-    pathcase = path+"\\"+"testcase"
+    pathcase = path+s+"testcase"
     filepaths = walk_dir_test(pathcase)
     func = lambda x: csvReadToDict(x)
     funcm = lambda x: dictToInfaceDict(x)
