31275475173e47a3a972eec200e42d0d36b6a1a9
litaojun <78069455@qq.com>
2019-04-16 23:55:56 +0800

    yaml read w update to r

--- opg/util/yamlOper.py
+++ opg/util/yamlOper.py
@@ -5,6 +5,7 @@ def dumpDataToYmlFile(filePath = None,data = None):
     with open(filePath, 'w', encoding="utf-8") as f:
          yaml.dump(data,f,Dumper=yaml.RoundTripDumper, default_flow_style=False, allow_unicode=True)
 
+
 def readYmlFile(filePath = None):
-    with open(filePath, 'w', encoding="utf-8") as f:
+    with open(filePath, 'r', encoding="utf-8") as f:
         return yaml.load(f.read(),Loader=yaml.Loader)
